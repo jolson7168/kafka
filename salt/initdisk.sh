4448a9e9df843211d73fed178e2d58749ab4af4b
Joe Olson <jolson7168@nododos.com>
2016-06-15 16:43:01 -0500

    removed sudos

--- salt/initdisk.sh
+++ salt/initdisk.sh
@@ -7,9 +7,9 @@ p
 
 
 w
-"|sudo fdisk $i;sudo mkfs.ext4 $i;done
-sudo mkdir /mnt/data
-sudo mount /dev/xvdb /mnt/data
+"|fdisk $i;mkfs.ext4 $i;done
+mkdir /mnt/data
+mount /dev/xvdb /mnt/data
 export data2="\/mnt\/data"
 export data="\/mnt"
 sed -i "0,/$data/s//$data2/" /etc/fstab

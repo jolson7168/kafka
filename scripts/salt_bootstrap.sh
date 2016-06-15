c1db130eb42f137a0ce5d7ddbc823abe0cf7fc0a
Joe Olson <jolson7168@nododos.com>
2016-06-15 16:26:51 -0500

    removed sudos

--- scripts/salt_bootstrap.sh
+++ scripts/salt_bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Use the following line if you booted the machine with a static IP
-sudo echo "127.0.0.1 ip-10-165-40-100" >>/etc/hosts
+echo "127.0.0.1 ip-10-165-40-100" >>/etc/hosts
 wget -O install_salt.sh https://bootstrap.saltstack.com
-sudo sh install_salt.sh
-sudo echo "file_client: local" >>/etc/salt/minion
-sudo echo "local: True" >>/etc/salt/minion
-sudo service salt-minion stop
-sudo apt-get install -y git
-sudo mkdir /srv/salt
+sh install_salt.sh
+echo "file_client: local" >>/etc/salt/minion
+echo "local: True" >>/etc/salt/minion
+service salt-minion stop
+apt-get install -y git
+mkdir /srv/salt
 git clone https://github.com/jolson7168/kafka.git /home/ec2-user/kafka
-sudo cp -R /home/ec2-user/kafka/salt/* /srv/salt
-sudo salt-call --local state.highstate --state-verbose=False
+cp -R /home/ec2-user/kafka/salt/* /srv/salt
+salt-call --local state.highstate --state-verbose=False

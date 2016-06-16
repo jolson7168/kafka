15bafecb7e9b8fc2993c224612975a3c09f6084a
Joe Olson <jolson7168@nododos.com>
2016-06-16 10:44:27 -0500

    updating

--- salt/top.sls
+++ salt/top.sls
@@ -1,4 +1,12 @@
+fileserver_backend:
+  - git
+
+gitfs_remotes:
+  - https://github.com/jolson7168/sun-java-formula.git
+
 base:
   '*':
     - osprep01
     - unzip
+    - sun-java
+
--- scripts/salt_bootstrap.sh
+++ scripts/salt_bootstrap.sh
@@ -2,13 +2,16 @@
 
 # Use the following line if you booted the machine with a static IP
 echo "127.0.0.1 ip-10-165-40-100" >>/etc/hosts
+yum -y update
+yum -y install wget
 wget -O install_salt.sh https://bootstrap.saltstack.com
 sh install_salt.sh
 echo "file_client: local" >>/etc/salt/minion
 echo "local: True" >>/etc/salt/minion
 service salt-minion stop
-apt-get install -y git
-mkdir /srv/salt
-git clone https://github.com/jolson7168/kafka.git /home/ec2-user/kafka
-cp -R /home/ec2-user/kafka/salt/* /srv/salt
+yum -y install -y git
+yum -y install -y python-pygit2
+sudo mkdir /srv/salt
+git clone https://github.com/jolson7168/kafka.git /home/centos/kafka
+sudo cp -R /home/centos/kafka/salt/* /srv/salt
 salt-call --local state.highstate --state-verbose=False
